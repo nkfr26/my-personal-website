892e3a0fa6463c237c758006a201fc6fa5ca4be4
nkfr26 <nkfr26@gmail.com>
2024-11-15 01:38:26 +0900

    feat: UDEVGothic35

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import { Header } from "@/components/custom/header";
 import "./globals.css";
+import localFont from "next/font/local";
+
+const lf = localFont({
+  src: "./fonts/UDEVGothic35-Regular.ttf",
+  variable: "--font-lf",
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +13,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja" style={{ scrollbarGutter: "stable" }}>
+    <html
+      lang="ja"
+      className={lf.variable}
+      style={{ scrollbarGutter: "stable" }}
+    >
       <body className="grid min-h-svh grid-rows-[auto_1fr] pb-9 font-mono">
         <Header />
         {children}
