dd24bf6a0e5804870947a44baae4a660cc15a406
nkfr26 <nkfr26@gmail.com>
2025-01-14 21:35:24 +0900

    feat: header shadow

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { BusinessCard } from "@/components/business-card";
 
 export default function Home() {
   return (
-    <main className="grid select-none place-content-center">
+    <main className="grid select-none place-content-center pb-12">
       <BusinessCard />
     </main>
   );
