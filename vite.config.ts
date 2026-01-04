697fb40076403cdcbe45da01f95a32cb87a11634
nkfr26 <n@rfu.jp>
2026-01-04 17:39:26 +0900

    nitro

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-// import { nitro } from 'nitro/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   plugins: [
     devtools(),
-    // nitro(),
+    nitro(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
